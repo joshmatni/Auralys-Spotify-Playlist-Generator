adab54cdd63e63cdaa472af875afcb3780a5561f
ploy <ployw@unr.edu>
2024-04-12 23:33:14 -0700

    chatbox

--- web/src/app/page.js
+++ web/src/app/page.js
@@ -3,16 +3,21 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main} >
-      <div className={styles.description}>
+      <div className={styles.background}>
         <header>
           <nav className={styles.header}>
             <div className={styles.headertext}>
               <h1 className={styles.title}>Auralys</h1>
+              <button className={styles.lgin} type="button"> Log In </button>
+              <button className={styles.sgnup} type="button"> Sign Up </button>
             </div>
           </nav>
         </header>
+          <div className={styles.hero}>
+            <div className={styles.chatbox}>
+              <input className={styles.inputbox} type="text" placeholder="Enter something we'll decide later"></input>
+            </div>
+          </div>
         </div>
-    </main>
   );
 }
