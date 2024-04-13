ae41c9b6c24f621afab7adeefc9141a5af307ac1
ploy <ployw@unr.edu>
2024-04-13 00:29:51 -0700

    basic home page

---
+++ web/src/app/main.js
@@ -0,0 +1,11 @@
+import $ from 'jquery';
+
+// $(function() {
+//     console.log( "ready!" );
+// });
+
+// function getInput(){
+//     $(".inputbox").on("change", function() {
+//         console.log($(".inputbox").val);
+//     })
+// }
--- web/src/app/page.js
+++ web/src/app/page.js
@@ -6,18 +6,35 @@ export default function Home() {
       <div className={styles.background}>
         <header>
           <nav className={styles.header}>
+            
             <div className={styles.headertext}>
               <h1 className={styles.title}>Auralys</h1>
-              <button className={styles.lgin} type="button"> Log In </button>
-              <button className={styles.sgnup} type="button"> Sign Up </button>
             </div>
+
+            <ul className={styles.headermenu}>
+
+            <li>
+            <button className={styles.lgin} type="navbutton"> Log In </button>
+              </li>
+
+              <li>
+              <button className={styles.sgnup} type="navbutton"> Sign Up </button>
+              </li>
+
+              <li>
+              <button className={styles.question} type="navbutton"> Contact Us </button>
+              </li>
+
+            </ul>
           </nav>
         </header>
           <div className={styles.hero}>
+
             <div className={styles.chatbox}>
-              <input className={styles.inputbox} type="text" placeholder="Enter something we'll decide later"></input>
+              <input className={styles.inputbox} type="text" placeholder="Enter prompt"></input>
             </div>
           </div>
         </div>
+        
   );
 }
