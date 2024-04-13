af31945957c8a3af9af2d6cdec89c744a73847d7
ploy <ployw@unr.edu>
2024-04-13 10:47:22 -0700

    update layout

--- web/src/app/main.js
+++ web/src/app/main.js
@@ -1,11 +1,3 @@
-import $ from 'jquery';
-
-// $(function() {
-//     console.log( "ready!" );
-// });
-
-// function getInput(){
-//     $(".inputbox").on("change", function() {
-//         console.log($(".inputbox").val);
-//     })
-// }
+export function generateResponse () {
+    console.log("hi");
+}
--- web/src/app/page.js
+++ web/src/app/page.js
@@ -1,12 +1,15 @@
+'use client';
 import Image from "next/image";
 import styles from "./page.module.css";
+import {generateResponse} from "./main";
 
 export default function Home() {
   return (
       <div className={styles.background}>
         <header>
           <nav className={styles.header}>
-            
+            <div className={styles.container}>
+
             <div className={styles.headertext}>
               <h1 className={styles.title}>Auralys</h1>
             </div>
@@ -26,15 +29,30 @@ export default function Home() {
               </li>
 
             </ul>
+            </div>
           </nav>
         </header>
           <div className={styles.hero}>
 
             <div className={styles.chatbox}>
-              <input className={styles.inputbox} type="text" placeholder="Enter prompt"></input>
+                <p className={styles.chatboxtext}>
+                  Enter a prompt!
+                </p>
+
+                <div className={styles.inputbox}>
+
+                <input className={styles.inputfield} type={styles.text} placeholder="Enter prompt"></input>
+                
+              <button type={styles.submit} className={styles.inputbutton} onClick={generateResponse}>
+                Submit
+              </button>
+                </div>
+
             </div>
           </div>
         </div>
+        
+
         
   );
 }
